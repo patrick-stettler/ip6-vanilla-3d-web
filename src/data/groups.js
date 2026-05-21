113039a9b44c85bb086b1818a36fcb71edd0a29c
Patrick Stettler <patrick.stettler@students.fhnw.ch>
2026-05-21 11:05:27 +0200

    data and css setup

---
+++ src/data/groups.js
@@ -0,0 +1,10 @@
+const MODULE_GROUPS = [
+    {id: 0, name: "Kontext"},
+    {id: 1, name: "Design"},
+    {id: 2, name: "Praxis"},
+    {id: 3, name: "Mathematik"},
+    {id: 4, name: "Informatik"},
+    {id: 5, name: "Management"}
+];
+
+export { MODULE_GROUPS };
---
+++ src/data/modules.js
@@ -0,0 +1,21 @@
+const MODULES = [
+    /* Design */
+    {id: 0, name: "Media Computing", code: "meco", group: 1, ects: 3, status: "", grade: null},
+
+    /* Management */
+    {id: 5, name: "Personal und Führung", code: "pefu", group: 5, ects: 3, status: "", grade: null},
+    
+    /* Mathematik */
+    {id: 10, name: "Mathematische Grundlagen der Informatik", code: "mgli", group: 3, ects: 3, status: "", grade: null},
+
+    /* Praxis */
+    {id: 19, name: "Projekt 1", code: "P1bb", group: 2, ects: 6, status: "", grade: null},
+
+    /* Informatik */
+    {id: 32, name: "Software Engineering Grundlagen", code: "sweGL", group: 4, ects: 6, status: "", grade: null},
+
+    /* Kontext */
+    {id: 64, name: "Schreibpraxis", code: "sprx", group: 0, ects: 2, status: "", grade: null}
+];
+
+export { MODULES };
